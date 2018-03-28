285537e4f781b5b6fed2e1cbeb9a116695309a0c
mariigrech <33154793+mariigrech@users.noreply.github.com>
2018-03-28 12:58:50 +0200

    Added comments and added code to check X variable

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -7,6 +7,28 @@ public class PaddleScript : MonoBehaviour {
 	public Rigidbody2D rb;
 	public float speed;
 		
+	void Update(){
+		// this input would go from -1 to 0 and to 1 when it moves from left to right
+		float x = Input.GetAxis("Horizontal");
+
+		//since we gave the stop variable a zero, I gave the code the directions in which it has to move.
+
+		//if x is smaller than zero that would mean that the controller would move to the left
+		if (x < 0){
+			MoveLeft();
+		}
+
+		//if x is larger than zero that would mean that the controller would move to the right
+		if (x > 0){
+			MoveRight();
+		}
+
+		//if x is equal to zero which is in the middle the paddle would stop in the middle
+		if (x == 0){
+			Stop ();
+		}
+	}
+	
 	void  MoveLeft(){
 		// the paddle will move to the left. It has a - (negative sign)
 		// infront of speed so now it would move to the left side of the screen
