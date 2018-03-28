a5cf480a4497c944883d38995234e233eb3cabad
mariigrech <33154793+mariigrech@users.noreply.github.com>
2018-03-28 11:05:35 +0200

    Added code to the paddle script, functions on how the paddle would move.

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed;
+		
+	void  MoveLeft(){
+		// the paddle will move to the left. It has a - (negative sign)
+		// infront of speed so now it would move to the left side of the screen
+		rb.velocity = new Vector2 (-speed,0);		
+	}
+
+
+	void  MoveRight(){
+		// the paddle will move with whatever value we give to the speed valuable.
+		// the paddle will move to the right side of the screen
+		rb.velocity = new Vector2 (speed,0);
+	}
+
+	void  Stop(){
+		// if no keys are pressed the paddle would stop moving to neither right or left.
+		rb.velocity = Vector2.zero;
+	}
+}
