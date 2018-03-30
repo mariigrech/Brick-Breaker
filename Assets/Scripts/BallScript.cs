483de8c8a98b3bd911182cf3762d55dc0af05684
mariigrech <33154793+mariigrech@users.noreply.github.com>
2018-03-30 11:27:57 +0200

    Set the  force to 20, set the parent as null so the ball would be able to play normally

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -16,6 +16,12 @@ public class BallScript : MonoBehaviour {
 	void Update () {
 		//whenever the space will be pressed the game would start
 		if(Input.GetKey(KeyCode.Space)){
+
+			//setting the parent to none whenever we press the space key for the first time, and then we add the force
+			transform.SetParent (null);
+			//it will get affected by physics foces again and afterwards we add the force (below)
+			rb.isKinematic = false;
+
 			rb.AddForce(new Vector2(ballForce, ballForce));
 		}
 	}
