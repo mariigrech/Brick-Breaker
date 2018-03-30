48c3add0079e44129f3daea6f14ba5e989a5ad74
mariigrech <33154793+mariigrech@users.noreply.github.com>
2018-03-30 12:03:24 +0200

    Added Bricks - Ball colliding working

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -6,6 +6,7 @@ public class BallScript : MonoBehaviour {
 
 	public Rigidbody2D rb;
 	public float ballForce;
+	bool gameStarted = false; //creating a boolean variable and then setting it to false, it will check if the game has started or not
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,10 @@ public class BallScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//whenever the space will be pressed the game would start
-		if(Input.GetKey(KeyCode.Space)){
+		if(Input.GetKey(KeyCode.Space)  && gameStarted == false)
+		//We are giving it a condition that if the space buttons has been pressed 
+		//AND the game hasn't yet started, then the code inside the if statement would be executed
+		{
 
 			//setting the parent to none whenever we press the space key for the first time, and then we add the force
 			transform.SetParent (null);
@@ -23,6 +27,7 @@ public class BallScript : MonoBehaviour {
 			rb.isKinematic = false;
 
 			rb.AddForce(new Vector2(ballForce, ballForce));
+			gameStarted = true; //the game has now started and no more force can be added to the ball
 		}
 	}
 }
