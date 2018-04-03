fb8e206bd4acccbd9c7f33a6b43457333f2685de
mariigrech <33154793+mariigrech@users.noreply.github.com>
2018-04-03 17:15:01 +0200

    Set background of the game, bricks breaking when they collide with the ball.

---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//When theres a collision between the ball and the bricks this method would be executed
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Ball") {
+			//everything that the Ball collides with will be destroyed
+			Destroy (gameObject);
+		}
+	}
+}
